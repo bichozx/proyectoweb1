c7667fdf2dd7bef8aba48729b5edb65ced8a4de3
bichozx <61300748+bichozx@users.noreply.github.com>
2020-07-23 21:22:56 -0500

    mi primer programa php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
+
 <?php
 
-echo ("generalidades del php")
-
+$cantidadestudianes=25;
+$cantidadprofesores=45;
+if($cantidadestudianes<$cantidadprofesores){
+    echo ("hay mas profesores que estudiante");
+}else{
+    echo("hay mas estudiantes");
+}
 ?>
